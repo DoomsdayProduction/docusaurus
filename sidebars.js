0910e78714e022fb060b148aa89561b0d36a52b0
DoomsdayProduction <Doomsdayxproductions@gmail.com>
2025-02-02 04:19:05 -0500

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -25,87 +25,89 @@ const sidebars = {
           label: "Payment Gateways",
           items: ["Carding-Methods/method-intro"],
         },
-    {
-      type: "category",
-      label: "Carding Methods",
-      items: [
         {
           type: "category",
-          label: "➡️ Direct Access Targets",
+          label: "Carding Methods",
           items: [
-            "Carding-Methods/➡️ Direct Access Targets/➡️ Direct Access Targets",
-          ],
-        },
-        {
-          type: "category",
-          label: "🎁💳 EGift Cards",
-          items: ["Carding-Methods/🎁💳 EGift Cards/🎁💳 EGift Cards"],
-        },
-        {
-          type: "category",
-          label: "🏦 AN RN Accepted",
-          items: [
-            "Carding-Methods/🏦 AN RN Accepted/🏦 AN RN Accepted",
-            "Carding-Methods/🏦 AN RN Accepted/APSX, LLC.",
-            "Carding-Methods/🏦 AN RN Accepted/TME",
-          ],
-        },
-        {
-          type: "category",
-          label: "👨‍🏭 Net-30 Sites",
-          items: [
-            "Carding-Methods/👨‍🏭 Net-30 Sites/👨‍🏭 Net-30 Sites",
-            "Carding-Methods/👨‍🏭 Net-30 Sites/Shirtsy",
-            "Carding-Methods/👨‍🏭 Net-30 Sites/Uline",
-          ],
-        },
-        {
-          type: "category",
-          label: "👻 Builds",
-          items: [
-            "Carding-Methods/👻 Builds/👻 Builds",
-            "Carding-Methods/👻 Builds/Husky Armory",
-          ],
-        },
-        {
-          type: "category",
-          label: "🔗 Link and Smack",
-          items: [
-            "Carding-Methods/🔗 Link and Smack/🔗 Link and Smack",
-            "Carding-Methods/🔗 Link and Smack/🎁 Light in The Box",
-            "Carding-Methods/🔗 Link and Smack/Meta",
-          ],
-        },
-        {
-          type: "category",
-          label: "🔰 Email Access Targets",
-          items: [
-            "Carding-Methods/🔰 Email Access Targets/🔰 Email Access Targets",
-            "Carding-Methods/🔰 Email Access Targets/💲 Important Documents & Finances",
-            "Carding-Methods/🔰 Email Access Targets/💳 Smacking with Payment on File",
-          ],
-        },
-        {
-          type: "category",
-          label: "🎰 Gambling",
-          items: [
-            "Carding-Methods/🎰 Gambling/🎰 Gambling",
-            "Carding-Methods/🎰 Gambling/Bookies",
-            "Carding-Methods/🎰 Gambling/Sweepstake Casinos",
-          ],
-        },
-        {
-          type: "category",
-          label: "🏠 AVS Enforced Sites",
-          items: [
-            "Carding-Methods/🏠 AVS Enforced Sites/🏠 AVS Enforced Sites",
-          ],
-        },
-        {
-          type: "category",
-          label: "🏦 Bank Specific Guides",
-          items: [
-            "Carding-Methods/🏦 Bank Specific Guides/🏦 Bank Specific Guides",
+            {
+              type: "category",
+              label: "➡️ Direct Access Targets",
+              items: [
+                "Carding-Methods/➡️ Direct Access Targets/➡️ Direct Access Targets",
+              ],
+            },
+            {
+              type: "category",
+              label: "🎁💳 EGift Cards",
+              items: ["Carding-Methods/🎁💳 EGift Cards/🎁💳 EGift Cards"],
+            },
+            {
+              type: "category",
+              label: "🏦 AN RN Accepted",
+              items: [
+                "Carding-Methods/🏦 AN RN Accepted/🏦 AN RN Accepted",
+                "Carding-Methods/🏦 AN RN Accepted/APSX, LLC.",
+                "Carding-Methods/🏦 AN RN Accepted/TME",
+              ],
+            },
+            {
+              type: "category",
+              label: "👨‍🏭 Net-30 Sites",
+              items: [
+                "Carding-Methods/👨‍🏭 Net-30 Sites/👨‍🏭 Net-30 Sites",
+                "Carding-Methods/👨‍🏭 Net-30 Sites/Shirtsy",
+                "Carding-Methods/👨‍🏭 Net-30 Sites/Uline",
+              ],
+            },
+            {
+              type: "category",
+              label: "👻 Builds",
+              items: [
+                "Carding-Methods/👻 Builds/👻 Builds",
+                "Carding-Methods/👻 Builds/Husky Armory",
+              ],
+            },
+            {
+              type: "category",
+              label: "🔗 Link and Smack",
+              items: [
+                "Carding-Methods/🔗 Link and Smack/🔗 Link and Smack",
+                "Carding-Methods/🔗 Link and Smack/🎁 Light in The Box",
+                "Carding-Methods/🔗 Link and Smack/Meta",
+              ],
+            },
+            {
+              type: "category",
+              label: "🔰 Email Access Targets",
+              items: [
+                "Carding-Methods/🔰 Email Access Targets/🔰 Email Access Targets",
+                "Carding-Methods/🔰 Email Access Targets/💲 Important Documents & Finances",
+                "Carding-Methods/🔰 Email Access Targets/💳 Smacking with Payment on File",
+              ],
+            },
+            {
+              type: "category",
+              label: "🎰 Gambling",
+              items: [
+                "Carding-Methods/🎰 Gambling/🎰 Gambling",
+                "Carding-Methods/🎰 Gambling/Bookies",
+                "Carding-Methods/🎰 Gambling/Sweepstake Casinos",
+              ],
+            },
+            {
+              type: "category",
+              label: "🏠 AVS Enforced Sites",
+              items: [
+                "Carding-Methods/🏠 AVS Enforced Sites/🏠 AVS Enforced Sites",
+              ],
+            },
+            {
+              type: "category",
+              label: "🏦 Bank Specific Guides",
+              items: [
+                "Carding-Methods/🏦 Bank Specific Guides/🏦 Bank Specific Guides",
+              ],
+            },
           ],
         },
       ],
